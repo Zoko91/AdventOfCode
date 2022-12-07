777a4046d048dc6be555bc8e4d20f5d464802f0c
Zoko91 <josephbeasse@gmail.com>
2022-12-07 16:13:59 +0100

    day7 - That might be my last day since it was super hard for me

---
+++ 2022/Day7/main.py
@@ -0,0 +1,65 @@
+# DAY 7
+
+with open('input.txt') as inp:
+    lignes = inp.readlines()
+for i in range(len(lignes)):
+    lignes[i] = lignes[i].rstrip()
+
+class File:
+    def __init__(self, name, size=0):
+        self.name = name
+        self.size = size
+        self.children = []
+        self.parent = None
+
+    def __str__(self):
+        return f"Name: {self.name}, size is {self.size}"
+
+
+sizes = {}
+firstFile = File('/')
+f = firstFile
+
+for i in range(len(lignes)):
+
+    if lignes[i].startswith('$'):
+        cmdLine = lignes[i].split()
+        ## Récupère toutes les parties de la ligne
+
+        cmd = cmdLine[1]
+        if cmd == 'cd':
+            directory = cmdLine[2]
+            ## Récupère le nom du dossier de la ligne
+
+            if directory == '..':
+                ## si c'est un cmd .. alors
+                f = f.parent
+            else:
+                file = File(directory)
+                file.parent = f
+                f.children.append(file)
+                f = file
+    else:
+        size, name = lignes[i].split()
+        # Only care if we are looking at a file.
+        if size != 'dir':
+            file = File(name, int(size))
+            f.children.append(file)
+
+
+def getDirSize(data):
+
+    if not data.children:
+        return data.size
+    totalsize = 0
+    for child in data.children:
+        totalsize += getDirSize(child)
+    sizes[data] = totalsize
+    return totalsize
+
+getDirSize(firstFile) # Pour remplir sizes
+print(len(sizes)) # taille de sizes
+
+# 1st star
+print(sum(sizeFile for sizeFile in sizes.values() if sizeFile < 100000)) # parcourt chaque valeur du dictionnaire
+# Answer : 1908462
