20c4026d07c0b0a79fde23bb4c15e8ebf647930e
Zoko91 <josephbeasse@gmail.com>
2022-12-10 09:51:45 +0100

    Second star day 10 - Very beautiful

--- 2022/Day10/main.py
+++ 2022/Day10/main.py
@@ -4,26 +4,51 @@ with open('input.txt') as inpt:
     data = inpt.read().strip()
 
 lines = [line for line in data.split('\n')]
-print(lines)
 
 cycles = ['1']
 X = 1
+CRT = " # "
+countCRT = 1
 for line in lines:
     if line.startswith('addx'):
         instruction, val = line.split()
+        # One cycle
+        if countCRT in [X-1, X, X+1]:
+            CRT += " # "
+        else:
+            CRT += " . "
         cycles.append(str(X))
+        countCRT += 1
+        if countCRT == 40:
+            CRT += '\n'
+            countCRT = 0
+
+        # Second cycle
         X += int(val)
+        if countCRT in [X-1, X, X+1]:
+            CRT += " # "
+        else:
+            CRT += " . "
+        countCRT += 1
+        if countCRT == 40:
+            CRT += '\n'
+            countCRT = 0
         cycles.append(str(X))
     else:
+        # Noop takes one cycle
+        if countCRT in [X-1, X, X+1]:
+            CRT += " # "
+        else:
+            CRT += " . "
+        countCRT += 1
+        if countCRT == 40:
+            CRT += '\n'
+            countCRT = 0
         cycles.append(str(X))
 
-print(cycles)
 
 signalStrength = 0
 for i in range(20, 221, 40):
-    print(i)
-    print(cycles[i-1])
-    print("------------------")
     signalStrength += int(cycles[i-1])*i
 
 # 1st star
@@ -32,7 +57,8 @@ print(signalStrength)
 
 
 # 2nd star
-
+CRT.split('\n')
+print(CRT)
 # Answer:
 
 
