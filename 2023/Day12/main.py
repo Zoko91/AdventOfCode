145ccb21865b4ec162848da6f8fc93a030b74c56
Zoko91 <josephbeasse@gmail.com>
2023-12-12 09:55:05 +0100

    2023 Day 12 part 1 submission

--- 2023/Day12/main.py
+++ 2023/Day12/main.py
@@ -5,20 +5,17 @@ lines = [line for line in data.split('\n')]
 
 def parse_input(line):
     conditions, groups = line.split(' ')
-    groups = tuple(map(int,groups.split(',')))
+    groups = list(map(int,groups.split(',')))
     return conditions, groups
 
 def find_combinations(cond, grp):
     '''
-    The function recursively explores all possibilities
-    - If Condition is empty, return 1 if there are no remaining groups otherwise return 0.
-    - If no remaining groups, return 0 if '#' is present in the condition otherwise return 1 because incompatible !
-    - Explore possibilities based on condition[0]:
-        - If '.' or '?'
-         => move to the next character and continue the recursion.
-        - If '#' or '?'
-         => Checks if group size is valid and there is no '.' before the group
-         => Recursively explore possibilities for  remaining parts of cond and remaining groups
+    Recursive function to explore all possibilities based on current letter of condition (., #, ?)
+    - If '.' or '?'
+        => move to the next character and continue the recursion
+    - If '#' or '?'
+        => checks if group size is valid and there is no '.' before the group
+        => recursively explore possibilities for  remaining parts of cond and remaining groups
     '''
     if not cond:
         if not grp:
@@ -36,11 +33,10 @@ def find_combinations(cond, grp):
         result += find_combinations(cond[1:], grp)
 
     if cond[0] in "#?":
-        if grp[0] <= len(cond) \
-                and "." not in cond[:grp[0]] \
-                and (grp[0] == len(cond)
-                     or cond[grp[0]] != "#"):
-            result += find_combinations(cond[grp[0] + 1:], grp[1:])
+        if grp[0] <= len(cond): # if length of current group not matching with the needed length then continue...
+            if "." not in cond[:grp[0]]: # if no '.' in the next length of block then still in current block, all good
+                if grp[0] == len(cond) or cond[grp[0]] != "#": # if end of block and not a # but rather . or ? then good
+                    result += find_combinations(cond[grp[0] + 1:], grp[1:])
 
     return result
 
